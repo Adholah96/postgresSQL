dcdbfa887302cb22069cde44a87585c82dc1666b
Adholah96 <adholah96@gmail.com>
2023-05-04 17:48:22 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -42,3 +42,12 @@ select avg(weight_kg) from animals;
 select neutered, sum(escape_attempts) from animals group by neutered;
 select species, min(weight_kg), max(weight_kg) from animals group by species;
 select species, avg(escape_attempts) from animals where date_of_birth between '1990-01-01' and '2000-12-31' group by species;
+
+
+select * from animals join owners on animals.owner_id = owners.id where owners.full_name = 'Melody Pond' ;
+select * from animals a join species s on a.species_id = s.id where s.name = 'Pokemon';
+select owners.*, animals.name as animal_name from owners left join animals on owners.id = animals.owner_id;
+select s.name, count(a.id) from species s left join animals a on s.id = a.species_id group by s.name;
+select a.* from owners o join animals a on o.id = a.owner_id join species s on a.species_id = s.id where o.full_name = 'Jennifer Orwell' and s.name = 'Digimon';
+select * from animals a join owners o on a.owner_id = o.id where o.full_name = 'Dean Winchester' and a.escape_attempts = 0;
+select o.full_name, count(a.*) as number_of_animals from owners o left join animals a on o.id = a.owner_id group by o.full_name order by number_of_animals desc;
