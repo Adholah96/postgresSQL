99d05c3b216e80e5a503c27f0e96efebb28405f6
Adholah96 <adholah96@gmail.com>
2023-05-05 22:35:26 +0300

    updated schema file

--- schema.sql
+++ schema.sql
@@ -36,3 +36,22 @@ ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
 
+CREATE TABLE vets(
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	name VARCHAR(250),
+	age INT,
+	date_of_graduation date
+);
+
+CREATE TABLE specializations(
+	vet_id INT REFERENCES vets(id),
+	species_id INT REFERENCES species(id),
+	PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits(
+	animal_id INT REFERENCES animals(id),
+	vet_id INT REFERENCES vets(id),
+	date_of_visit date,
+	PRIMARY KEY (animal_id, vet_id, date_of_visit)
+);
